93d78dfe71a9884fa08b51cdbd966d411c28c08e
Alex Volkov <vol4ikman@gmail.com>
2016-04-17 20:51:49 +0300

    Update jquery.alttag.0.1.js

--- jquery.alttag.0.1.js
+++ jquery.alttag.0.1.js
@@ -1,47 +1,58 @@
+jQuery(document).ready(function(){
+
+    jQuery("body").altTag({
+        test: true,
+        roleLink: true
+    });
+
+});
+
 (function ( $ ) {
 
     $.fn.altTag = function( options ) {
 
         //Dafaults
         var settings = $.extend({
-            test : false,
-            fix  : true,
-            alt  : 'image'
+            test:     false,
+            fix:      true,
+            alt:      'image',
+            roleLink: false
         }, options );
 
-        //Run test if enabled
-        if( settings.test ) {
+        var noAlt    = [];
+        var emptyAlt = [];
+        var withAlt  = [];
 
-            var noAlt    = [];
-            var emptyAlt = [];
-            var withAlt  = [];
-
-            jQuery("body img").each(function(index,value){
-
-                var imageElement = jQuery(this);
-
+        jQuery("body img").each(function(index,value){
+            var imageElement = jQuery(this);
+            //Fix missing alt tags with default image string
+            if( settings.fix ) {
+                if( !imageElement.attr("alt") ){
+                    imageElement.attr("alt",settings.alt)
+                }
+            }
+            //Run test if enabled
+            if( settings.test ) {
                 if( imageElement.attr("alt") ){
                     withAlt.push(index);
                 } else {
                     noAlt.push(index);
                 }
-            });
+            }
+        });
 
+        if( settings.test ) {
             console.log("total images without 'alt' tag: " + noAlt.length);
             console.log("total images with 'alt' tag: " + withAlt.length);
         }
 
-        //Fix missing alt tags with default image string
-        if( settings.fix ) {
-
-            jQuery("body img").each(function(index,value){
-                var imageElement = jQuery(this);
-                if( !imageElement.attr("alt") ){
-                    imageElement.attr("alt",settings.alt)
-                }
+        //Add role link to each <a> tag
+        if( settings.roleLink ) {
+            jQuery("body a").each(function(index,value){
+                jQuery(this).attr("role","link");
             });
-
         }
+
 
     };
 
